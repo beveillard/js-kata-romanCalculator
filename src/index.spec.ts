15462b7369326972bfd5b849d997baf1491f7a63
Benoît Eveillard <benoit.eveillard@gmail.com>
2021-03-06 09:56:20 -0500

    feature(RomanCalculator)

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,71 @@
-import { systemUnderTest } from ".";
+import {
+  expandRomanNumber,
+  condenseRomanNumber,
+  canonifyRomanNumber,
+  addExpendedRomanNumbers,
+  addRomanNumbers,
+} from ".";
 
-describe("Test of systemUnderTest()", function() {
-  it("Should pass", function() {
-    expect(systemUnderTest()).toEqual(true);
+describe("expandRomanNumber", function () {
+
+  it("expands CXCIX (199)", function () {
+    expect(expandRomanNumber('CXCIX')).toEqual('CLXXXXVIIII');
   });
+
+  it("expands CCCXCVIII (398)", function () {
+    expect(expandRomanNumber('CCCXCVIII')).toEqual('CCCLXXXXVIII');
+  });
+
+  it("expands MMMCMXCIX (3999)", function () {
+    expect(expandRomanNumber('MMMCMXCIX')).toEqual('MMMDCCCCLXXXXVIIII');
+  });
+
+});
+
+describe("symplifyRomanNumber", function () {
+
+  it("symplify 199", function () {
+    expect(condenseRomanNumber('I'.repeat(199))).toEqual('CLXXXXVIIII');
+  });
+
+  it("symplify 398", function () {
+    expect(condenseRomanNumber('I'.repeat(398))).toEqual('CCCLXXXXVIII');
+  });
+
+  it("symplify 3999", function () {
+    expect(condenseRomanNumber('I'.repeat(3999))).toEqual('MMMDCCCCLXXXXVIIII');
+  });
+
+});
+
+describe("canonifyRomanNumber", function () {
+
+  it("canonifies 'CLXXXXVIIII' (199)", function () {
+    expect(canonifyRomanNumber('CLXXXXVIIII')).toEqual('CXCIX');
+  });
+
+  it("canonifies 'CCCLXXXXVIII' (398)", function () {
+    expect(canonifyRomanNumber('CCCLXXXXVIII')).toEqual('CCCXCVIII');
+  });
+
+  it("canonifies MMMDCCCCLXXXXVIIII (3999)", function () {
+    expect(canonifyRomanNumber('MMMDCCCCLXXXXVIIII')).toEqual('MMMCMXCIX');
+  });
+
+});
+
+describe("addExpendedRomanNumbers", function () {
+
+  it("add 'CLXXXXVIIII' + 'CLXXXXVIIII' (199+199)", function () {
+    expect(addExpendedRomanNumbers('CLXXXXVIIII', 'CLXXXXVIIII')).toEqual('CCLLXXXXXXXXVVIIIIIIII');
+  });
+
+});
+
+describe("addRomanNumbers", function () {
+
+  it("add 'CXCIX' + 'CXCIX' (199+199)", function () {
+    expect(addRomanNumbers('CXCIX', 'CXCIX')).toEqual('CCCXCVIII');
+  });
+
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,54 @@
-export function systemUnderTest(): boolean {
-  return true;
+export function addRomanNumbers(number1: string, number2: string) {
+  let expendedSum = addExpendedRomanNumbers(
+    expandRomanNumber(number1),
+    expandRomanNumber(number2)
+  );
+  return canonifyRomanNumber(condenseRomanNumber(expendedSum));
+}
+
+export function expandRomanNumber(number: string) {
+  return number
+    .replace('CM', 'DCCCC')
+    .replace('CD', 'CCCC')
+    .replace('XC', 'LXXXX')
+    .replace('XL', 'XXXX')
+    .replace('IX', 'VIIII')
+    .replace('IV', 'IIII');
+}
+
+export function condenseRomanNumber(expNumber: string) {
+  let candidate = expNumber
+    .replace('IIIII', 'V')
+    .replace('VV', 'X')
+    .replace('XXXXX', 'L')
+    .replace('LL', 'C')
+    .replace('CCCCC', 'D')
+    .replace('DD', 'M');
+
+  if (candidate != expNumber)
+    return condenseRomanNumber(candidate);
+  return candidate;
+}
+
+export function canonifyRomanNumber(expNumber: string) {
+  return expNumber
+    .replace('DCCCC', 'CM')
+    .replace('CCCC', 'CD')
+    .replace('LXXXX', 'XC')
+    .replace('XXXX', 'XL')
+    .replace('VIIII', 'IX')
+    .replace('IIII', 'IV');
+}
+
+export function addExpendedRomanNumbers(number1: string, number2: string) {
+  let concatNumbers = number1 + number2;
+  let sum =
+    (concatNumbers.match(/M+/g) || "").toString().replace(",", "") +
+    (concatNumbers.match(/D+/g) || "").toString().replace(",", "") +
+    (concatNumbers.match(/C+/g) || "").toString().replace(",", "") +
+    (concatNumbers.match(/L+/g) || "").toString().replace(",", "") +
+    (concatNumbers.match(/X+/g) || "").toString().replace(",", "") +
+    (concatNumbers.match(/V+/g) || "").toString().replace(",", "") +
+    (concatNumbers.match(/I+/g) || "").toString().replace(",", "");
+  return sum;
 }
