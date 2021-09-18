d4fb3bad51a547cc495bbd76e08ca926295765e9
Benoît Eveillard <benoit.eveillard@gmail.com>
2021-09-18 09:12:41 -0400

    RomanCalculator

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,71 @@
-import { systemUnderTest } from ".";
+import {
+  canonifyRomanNumber,
+  deCanonifyRomanNumber,
+  condenseRomanNumber,
+  addNonCanonicalRomanNumbers,
+  addRomanNumbers,
+} from ".";
 
-describe("Test of systemUnderTest()", function() {
-  it("Should pass", function() {
-    expect(systemUnderTest()).toEqual(true);
+describe("deCanonifyRomanNumber", function () {
+
+  it("decanonify CXCIX (199)", function () {
+    expect(deCanonifyRomanNumber('CXCIX')).toEqual('CLXXXXVIIII');
   });
+
+  it("decanonify CCCXCVIII (398)", function () {
+    expect(deCanonifyRomanNumber('CCCXCVIII')).toEqual('CCCLXXXXVIII');
+  });
+
+  it("decanonify MMMCMXCIX (3999)", function () {
+    expect(deCanonifyRomanNumber('MMMCMXCIX')).toEqual('MMMDCCCCLXXXXVIIII');
+  });
+
+});
+
+describe("condenseRomanNumber", function () {
+
+  it("condense 199", function () {
+    expect(condenseRomanNumber('I'.repeat(199))).toEqual('CLXXXXVIIII');
+  });
+
+  it("condense 398", function () {
+    expect(condenseRomanNumber('I'.repeat(398))).toEqual('CCCLXXXXVIII');
+  });
+
+  it("condense 3999", function () {
+    expect(condenseRomanNumber('I'.repeat(3999))).toEqual('MMMDCCCCLXXXXVIIII');
+  });
+
+});
+
+describe("canonifyRomanNumber", function () {
+
+  it("canonify 'CLXXXXVIIII' (199)", function () {
+    expect(canonifyRomanNumber('CLXXXXVIIII')).toEqual('CXCIX');
+  });
+
+  it("canonify 'CCCLXXXXVIII' (398)", function () {
+    expect(canonifyRomanNumber('CCCLXXXXVIII')).toEqual('CCCXCVIII');
+  });
+
+  it("canonify MMMDCCCCLXXXXVIIII (3999)", function () {
+    expect(canonifyRomanNumber('MMMDCCCCLXXXXVIIII')).toEqual('MMMCMXCIX');
+  });
+
+});
+
+describe("addNonCanonicalRomanNumbers", function () {
+
+  it("add 'CLXXXXVIIII' + 'CLXXXXVIIII' (199+199)", function () {
+    expect(addNonCanonicalRomanNumbers('CLXXXXVIIII', 'CLXXXXVIIII')).toEqual('CCLLXXXXXXXXVVIIIIIIII');
+  });
+
+});
+
+describe("addRomanNumbers", function () {
+
+  it("add 'CXCIX' + 'CXCIX' (199+199)", function () {
+    expect(addRomanNumbers('CXCIX', 'CXCIX')).toEqual('CCCXCVIII');
+  });
+
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,87 @@
-export function systemUnderTest(): boolean {
-  return true;
+type RomanNumber = string;
+
+type MatchPattern = RegExp;
+
+interface CanonicalEquivalence {
+  canonical: string;
+  equivalent: string;
+}
+
+interface Equivalence {
+  condensed: string;
+  expanded: string;
+}
+
+const CANONICAL_EQUIVALENCES: CanonicalEquivalence[] = [
+  { canonical: "CM", equivalent: "DCCCC" },
+  { canonical: "CD", equivalent: "CCCC" },
+  { canonical: "XC", equivalent: "LXXXX" },
+  { canonical: "XL", equivalent: "XXXX" },
+  { canonical: "IX", equivalent: "VIIII" },
+  { canonical: "IV", equivalent: "IIII" },
+]
+
+const EQUIVALENCES: Equivalence[] = [
+  { expanded: "IIIII", condensed: "V" },
+  { expanded: "VV", condensed: "X" },
+  { expanded: "XXXXX", condensed: "L" },
+  { expanded: "LL", condensed: "C" },
+  { expanded: "CCCCC", condensed: "D" },
+  { expanded: "DD", condensed: "M" },
+]
+
+const MATCH_PATTERNS: MatchPattern[] = [
+  /M*/g,
+  /D*/g,
+  /C*/g,
+  /L*/g,
+  /X*/g,
+  /V*/g,
+  /I*/g,
+];
+
+export function addRomanNumbers(number1: RomanNumber, number2: RomanNumber): RomanNumber {
+  const expandedSum = addNonCanonicalRomanNumbers(deCanonifyRomanNumber(number1), deCanonifyRomanNumber(number2));
+  const condensedSum = condenseRomanNumber(expandedSum);
+  return canonifyRomanNumber(condensedSum);
+}
+
+export function canonifyRomanNumber(number: RomanNumber): RomanNumber {
+  function canonify(number: RomanNumber, { canonical, equivalent }): RomanNumber {
+    return number.replace(equivalent, canonical);
+  }
+
+  return CANONICAL_EQUIVALENCES.reduce(canonify, number);
+}
+
+export function deCanonifyRomanNumber(number: RomanNumber): RomanNumber {
+  function deCanonify(number: RomanNumber, { canonical, equivalent }): RomanNumber {
+    return number.replace(canonical, equivalent);
+  }
+
+  return CANONICAL_EQUIVALENCES.reduce(deCanonify, number);
+}
+
+export function condenseRomanNumber(number: RomanNumber): RomanNumber {
+  function condense(number: RomanNumber, { condensed, expanded }): RomanNumber {
+    return number.replace(expanded, condensed);
+  }
+
+  const candidate = EQUIVALENCES.reduce(condense, number);
+
+  if (candidate != number) {
+    return condenseRomanNumber(candidate);
+  }
+
+  return candidate;
+}
+
+export function addNonCanonicalRomanNumbers(number1: RomanNumber, number2: RomanNumber): RomanNumber {
+  const concatNumbers = number1 + number2;
+
+  function construct(number: RomanNumber, pattern: MatchPattern): RomanNumber {
+    return number + concatNumbers.match(pattern).join("");
+  }
+
+  return MATCH_PATTERNS.reduce(construct, "");
 }
